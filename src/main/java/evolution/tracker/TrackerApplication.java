7caa4c280cd4d5064897bf180b87c6ac5dd9e788
Boeing1337 <CK0T060UHR@GMAIL.COM>
2020-08-14 04:15:29 +0300

    Adding java docs

--- src/main/java/evolution/tracker/TrackerApplication.java
+++ src/main/java/evolution/tracker/TrackerApplication.java
@@ -3,10 +3,18 @@ package evolution.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Reactive REST API application for CRUD operations.
+ */
 @SpringBootApplication
 public class TrackerApplication {
 
-    public static void main(String[] args) {
+    /**
+     * Main.
+     *
+     * @param args the args
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(TrackerApplication.class, args);
     }
 
